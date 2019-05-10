aec325e69d408220e31935e433a1a8c108491ea0
Laukik On Windows Home <22937426+LRagji@users.noreply.github.com>
2019-05-11 00:36:02 +0530

    Added City name vs Ids for known ones

---
+++ src/CityMap.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "Serialized": [['Mumbai', '1275339'],
+    ['Dubai', '292223'],
+    ['Berlin', '2950159'],
+    ['Rome', '6539761'],
+    ['City of london', '2643741'],
+    ['Paris', '2988507'],
+    ['Dublin', '2964574'],
+    ['Washington', '4229546'],
+    ['Delhi', '1273294'],
+    ['New York', '5128581']]
+}
